411fc371f516c0abba2c77aa5c1b6a30d016d5e4
Martin Macharia <mosmartin5@gmail.com>
2025-06-20 21:02:44 +0200

    feat(auth): create SignIn page with GitHub authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
